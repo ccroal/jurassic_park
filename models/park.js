2c9442759032e65fab93c95229e5350aea462a5d
Christopher Croal <ccroal@ntlworld.com>
2018-12-19 11:03:19 +0000

    refactored with homework solutions

--- models/park.js
+++ models/park.js
@@ -1,7 +1,7 @@
 const Park = function(name, ticketPrice, dinosaurs){
   this.name = name;
   this.ticketPrice = ticketPrice;
-  this.dinosaurs = dinosaurs
+  this.dinosaurs = dinosaurs;
 };
 
 
@@ -10,29 +10,47 @@ Park.prototype.addDinosaur = function(dinosaur){
 };
 
 Park.prototype.deleteDinosaur = function(dinosaur){
-  this.dinosaurs.shift()
+  const index = this.dinosaurs.indexOf(dinosaur);
+  this.dinosaurs.splice(index, 1)
 };
 
 Park.prototype.mostVisitors = function(){
-  let dinosaurs = this.dinosaurs
-  const result = Math.max(dinosaurs.guestsAttractedPerDay)
-    for (dinosaur of dinosaurs){
-      if (dinosaur.guestsAttractedPerDay === result){
-      return dinosaur.species
-    }
-    console.log(result);
-  }
+  let sorted = this.dinosaurs.sort(function(dinosaura, dinosaurb){
+    return dinosaurb.guestsAttractedPerDay - dinosaura.guestsAttractedPerDay
+  });
+  return sorted[0];
 };
 
-Park.prototype.findDinosaurs = function(species){
-  for(dinosaur of this.dinosaurs){
+Park.prototype.findSpecies = function(species){
+  const result = []
+  for(let dinosaur of this.dinosaurs){
     if (species === dinosaur.species){
-      return dinosaur
+      result.push(dinosaur)
     }
   }
+  return result;
 };
 
-Park.prototype.findDinosaurs = function(){};
+Park.prototype.numberOfDailyVisitors = function(){
+  let total = 0
+  for (let dinosaur of this.dinosaurs){
+    total += dinosaur.guestsAttractedPerDay;
+  }
+  return total;
+}
 
+Park.prototype.numberOfYearlyVisitors = function(){
+  return this.numberOfDailyVisitors() * 365;
+}
 
-module.exports = Park
+Park.prototype.calculateYearlyRevenue = function(){
+  return this.numberOfYearlyVisitors() * this.ticketPrice;
+}
+
+Park.prototype.removeBySpecies = function(species){
+  const allSpecies = this.findSpecies(species);
+  for (let dinosaur of allSpecies){
+    this.deleteDinosaur(dinosaur);
+  }
+}
+module.exports = Park;
